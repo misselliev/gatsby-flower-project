b01a27409d47fe2956fdd70523f16c3cc436ac38
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-06-15 19:03:56 -0700

    add navbar and basic css

---
+++ src/components/Globals/Navbar.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react"
+import { Link } from "gatsby"
+import flower from "../../images/flower.svg"
+import { FaCartArrowDown } from "react-icons/fa"
+
+export default class Navbar extends Component {
+  state = {
+    navbarOpen: false,
+    css: "collapse navbar-collapse",
+    links: [
+      {
+        id: 1,
+        path: "/",
+        text: "home",
+      },
+      {
+        id: 2,
+        path: "/about",
+        text: "about",
+      },
+    ],
+  }
+  navbarHandler = () => {
+    this.state.navbarOpen
+      ? this.setState({ navbarOpen: false, css: "collapse navbar-collapse" })
+      : this.setState({ navbarOpen: true, css: "collapsenavbar-collapse show" })
+  }
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm bg-light navbar-light">
+        <Link to="/" className="navbar-brand">
+          <img src={flower} alt="flower" class="square" />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={this.navbarHandler}
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className={this.state.css}>
+          <ul className="navbar-nav mx-auto">
+            {this.state.links.map(link => {
+              return (
+                <li key={link.id} className="nav-item">
+                  <Link to={link.path} className="nav-link text-capitalize">
+                    {link.text}
+                  </Link>
+                </li>
+              )
+            })}
+            <li className="nav-item ml-sm-5">
+              <FaCartArrowDown className="cart-icon" />
+            </li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -10,9 +10,14 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 import "./bootstrap.min.css"
 import "./layout.css"
+import Navbar from "./Globals/Navbar"
+const Layout = ({ children }) => (
+  <>
+    <Navbar />
 
-const Layout = ({ children }) => <>{children}</>
-
+    {children}
+  </>
+)
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
